d5092b6f496dcef8f348ee67bc8ee977abe55c72
takemasa1110 <44985717+takemasa1110@users.noreply.github.com>
2020-07-29 13:58:53 +0900

    Fixed file list order issue and hidden directory treatment

--- CheckMedia.bat
+++ CheckMedia.bat
@@ -3,13 +3,18 @@
 set CmdName=CertUtil
 set HashMethod=MD5
 set MasterFile=CheckMediaMaster.txt
+set ResultTempFile=%TEMP%\CheckMediaResultTemp.txt
 set ResultFile=%TEMP%\CheckMediaResult.txt
 set CurrentPath=%~dp0
+set WPSettingsFile=System Volume Information\WPSettings.dat
+set IndexerVolumeGuidFile=System Volume Information\IndexerVolumeGuid
 
 pushd %CurrentPath%
 
+if exist %ResultTempFile% del %ResultTempFile%
 if exist %ResultFile% del %ResultFile%
 for /R .\ %%f in (*.*) do call :PrintFileHash "%%~f"
+sort /+34 < %ResultTempFile% > %ResultFile%
 echo %~nx0:
 if exist %MasterFile% (
 	fc %MasterFile% %ResultFile% >NUL && echo PASSED || echo FAILED
@@ -30,11 +35,14 @@ if "%Hash%" == "" if "%~z1" == "0" (
 ) else (
 	exit /b 0
 )
-if "%~nx1" == "%MasterFile%" exit /b 0
+
 set Hash=%Hash: =%
 setlocal EnableDelayedExpansion
 set PathToConvert=%~f1
 set FilePath=!PathToConvert:*%CurrentPath%=!
+if "%FilePath%" == "%MasterFile%" exit /b 0
+if "%FilePath%" == "%WPSettingsFile%" exit /b 0
+if "%FilePath%" == "%IndexerVolumeGuidFile%" exit /b 0
 echo %Hash% %FilePath%
-echo %Hash% %FilePath% >> %ResultFile%
+echo %Hash% %FilePath% >> %ResultTempFile%
 exit /b 0
